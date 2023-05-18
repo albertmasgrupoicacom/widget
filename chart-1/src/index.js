a87cd6742aae3320ffa20093bd7898b91a075522
Albert Mas <albert.mas@grupoica.com>
2023-05-18 08:24:53 +0200

    FEAT: [stackedBar] - changes

--- chart-1/src/index.js
+++ chart-1/src/index.js
@@ -8,38 +8,63 @@
  * @return {void}
  */
 import Chart from 'chart.js/auto';
+import mock from './mono_var.json';
+
 export default function main({portletNamespace, contextPath, portletElementId}) {
-    
+    // fetch('./assets/mock.json')
+    //     .then((response) => response.json())
+    //     .then((json) => console.log(json));
+    console.log(mock);
+
+    let data_extract = {};
+    data_extract.filas = mock.filas;
+
     const node = document.getElementById(portletElementId);
 
     node.innerHTML =`<canvas id="chart" width="400" height="400"></canvas>`;
 
         const ctx = document.getElementById('chart');
+        const labels = ['Enero','Febrero','Marzo'];
         const myChart = new Chart(ctx, {
             type: 'bar',
-            data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
+            data :{
+            labels: labels,
+            datasets: [
+                {
+                label: 'Dataset 1',
+                data: [1,5,8],
+                backgroundColor: 'yellow',
+                },
+                {
+                label: 'Dataset 2',
+                data: [1,5,8],
+                backgroundColor: 'red',
+                },
+                {
+                label: 'Dataset 3',
+                data: [1,5,8],
+                backgroundColor: 'green',
+                },
+            ]
+            },
+            options: {
+                plugins: {
+                title: {
+                    display: true,
+                    text: 'Chart.js Bar Chart - Stacked'
+                },
+                },
+                responsive: true,
+                scales: {
+                x: {
+                    stacked: true,
+                },
+                y: {
+                    min: 0,
+                    max: 25,
+                    stacked: true
+                }
+                }
             }
         });
         
