e21c54b577f8165eab54d13696d1deed33aa1e7e
Albert Mas <albert.mas@grupoica.com>
2023-06-20 11:21:11 +0200

    FEAT: [vars] - declare graphinc to local & add templates

--- src/index.js
+++ src/index.js
@@ -8,19 +8,10 @@ export default function main({portletNamespace, contextPath, portletElementId,co
 
     const node = document.getElementById(portletElementId);
     node.innerHTML =`
-        <div>
-            <button id="exportExcelButton">Excel</button>
-            <button id="exportPdfButton">PDF</button>
-        </div>
         <div id="graph_page" class="cis-caja-tot"></div>
     `;
 
-    let excelButton = document.getElementById("exportExcelButton");
-    if(excelButton) {excelButton.onclick = (event => graphic.exportExcel())}
-
-    let pdfButton = document.getElementById("exportPdfButton");
-    if(pdfButton) {pdfButton.onclick = (event => graphic.exportPdf())}
-    
+    let graphic; // comentar
     switch (dataService.type) {
         case 'SERIE':
             graphic = new SerieChart();
